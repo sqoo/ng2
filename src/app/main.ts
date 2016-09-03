965b4f119af096143419ae8f39fa47691b7ff20e
sqoo <djspooxxx@gmail.com>
2016-09-03 17:45:07 +0100

    first

---
+++ src/app/main.ts
@@ -0,0 +1,7 @@
+/* Avoid: 'error TS2304: Cannot find name <type>' during compilation */
+///<reference path="../../typings/index.d.ts"/>
+
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {AppComponent} from "./app.component";
+
+bootstrap(AppComponent);
